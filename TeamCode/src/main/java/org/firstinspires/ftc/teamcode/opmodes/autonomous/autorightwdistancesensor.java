298e7b63cb13afcc4fb51c7c209d280b7d4e7483
saa938 <83087832+saa938@users.noreply.github.com>
2023-08-20 15:58:17 -0700

    competitive programming > nikhil

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/autorightwdistancesensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/autorightwdistancesensor.java
@@ -237,9 +237,6 @@ public class autorightwdistancesensor extends LinearOpMode
         telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
         telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
         telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
-        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
-        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
-        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
     }
     public void straight(double power, int time) {
         robot.leftfrontmotor.setPower(power);
